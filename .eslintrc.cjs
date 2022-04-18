5d53f3f8d8265ce45092602ec94f4fa4a7783a1d
gymcoding <bruce.lean17@gmail.com>
2022-04-18 23:36:34 +0900

    Set ESLint, Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,30 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier"
-  ],
-  "env": {
-    "vue/setup-compiler-macros": true
-  }
-}
+	root: true,
+	extends: [
+		'plugin:vue/vue3-essential',
+		'eslint:recommended',
+		'@vue/eslint-config-prettier',
+	],
+	env: {
+		'vue/setup-compiler-macros': true,
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+			},
+		],
+	},
+};
