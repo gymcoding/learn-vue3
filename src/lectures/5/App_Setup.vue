5e3cfc2e355d1732625b65e7e582e83977d3d14c
gymcoding <bruce.lean17@gmail.com>
2022-04-20 22:59:43 +0900

    5_setup

---
+++ src/lectures/5/App_Setup.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<p>
+			{{ counter }}
+		</p>
+		<p>
+			{{ message }}
+		</p>
+		<button @click="increment">click!</button>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const counter = ref(0);
+		const message = ref('Hello Vue3');
+		const increment = () => {
+			counter.value++;
+		};
+		return {
+			counter,
+			message,
+			increment,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
