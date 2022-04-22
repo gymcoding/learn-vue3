8f54038be41f2aa77c4acd5dcf324e57c6bdd57a
gymcoding <bruce.lean17@gmail.com>
2022-04-22 11:52:22 +0900

    7 update App_ref.vue

--- src/lectures/7/App_ref.vue
+++ src/lectures/7/App_ref.vue
@@ -1,43 +1,24 @@
 <template>
 	<div>
-		<h2>State Message</h2>
-		<p>{{ state.message }}</p>
-		<button v-on:click="addStateMessage">click</button>
-		<!-- <h2>Message</h2>
-		<p>{{ message.value }}</p>
-		<button v-on:click="addMessage">click</button> -->
-		<h2>Ref: Messsage</h2>
 		<p>{{ message }}</p>
-		<button v-on:click="addMessage">click</button>
+		<button v-on:click="addMessage">add click</button>
 	</div>
 </template>
 
 <script>
-import { reactive, ref } from 'vue';
+import { ref } from 'vue';
 
 export default {
 	setup() {
-		const state = reactive({
-			message: 'Hello Vue3',
-		});
-		const addStateMessage = () => {
-			state.message = state.message + '!';
-		};
+		let message = ref('Hello Vue!');
 
-		// let message = reactive({
-		// 	value: 'Hello Vue3',
-		// });
-		// const addMessage = () => {
-		// 	message.value = message.value + '!';
-		// };
-
-		const message = ref('Hello Ref!');
 		const addMessage = () => {
 			message.value = message.value + '!';
 		};
+		console.log(message);
+		console.log('message: ', message.value);
+		console.log('message typeof: ', typeof message.value);
 		return {
-			state,
-			addStateMessage,
 			message,
 			addMessage,
 		};
