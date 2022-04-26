8c8634d3315258da28431f2bc29aad406ee732f0
gymcoding <bruce.lean17@gmail.com>
2022-04-26 15:36:19 +0900

    12_directives

---
+++ src/lectures/12/App_directives.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<p>{{ msg }}</p>
+		<p v-text="msg"></p>
+		<p v-html="htmlStr"></p>
+		<p v-text="htmlStr"></p>
+		<p v-pre>{{ msg 안녕하세요 }}</p>
+		<p v-once>{{ msg }} !!!!</p>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const msg = ref('안녕하세요');
+		const htmlStr = ref('<strong>안녕!!!</strong>');
+
+		return { msg, htmlStr };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/lectures/12/App_v-memo.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<div v-memo="[views, likes]">
+			<p>subscribers: {{ subscribers }}</p>
+			<p>views: {{ views }}</p>
+			<p>likes: {{ likes }}</p>
+		</div>
+		<button @click="subscribers++">Subs++</button>
+		<button @click="views++">Views++</button>
+		<button @click="likes++">Likes++</button>
+		<div>
+			<p>subscribers: {{ subscribers }}</p>
+			<p>views: {{ views }}</p>
+			<p>likes: {{ likes }}</p>
+		</div>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const subscribers = ref(4000);
+		const views = ref(400);
+		const likes = ref(20);
+		return { subscribers, views, likes };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
