c835a56f85c97fc98a1b2f1bc64e3aa4a3828c52
gymcoding <bruce.lean17@gmail.com>
2022-04-29 18:03:39 +0900

    18_single_file_component

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="card">
 		<div class="card-body">
-			<h5 class="card-title">Card title</h5>
+			<h5 class="card-title red">Card title</h5>
 			<p class="card-text">
 				Some quick example text to build on the card title and make up the bulk
 				of the card's content.
@@ -12,13 +12,34 @@
 </template>
 
 <script>
+import { ref, useCssModule } from 'vue';
+
 console.log('AppCard module');
 export default {
 	setup() {
+		// const style = useCssModule();
+		// console.log('style: ', style);
 		console.log('AppCard setup()');
-		return {};
+
+		const color = ref('red');
+		// color.value = 'yellow';
+		return { color };
 	},
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+.red {
+	color: v-bind(color) !important;
+}
+</style>
+<!-- <style module="classes">
+.red {
+	color: red !important;
+}
+</style> -->
+<!-- <style scoped>
+.red {
+	color: red !important;
+}
+</style> -->
