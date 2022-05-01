a34dd0fa9ef43455c4874394c1b6d9bcaf58a462
gymcoding <bruce.lean17@gmail.com>
2022-05-02 02:15:04 +0900

    19_props

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,45 +1,63 @@
 <template>
 	<div class="card">
 		<div class="card-body">
-			<h5 class="card-title red">Card title</h5>
-			<p class="card-text">
-				Some quick example text to build on the card title and make up the bulk
-				of the card's content.
-			</p>
-			<a href="#" class="btn btn-primary">Go somewhere</a>
+			<!-- type : news , notice -->
+			<span class="badge bg-secondary">{{ typeName }}</span>
+			<h5 class="card-title red mt-2">{{ title }}</h5>
+			<p class="card-text">{{ contents }}</p>
+			<a href="#" class="btn" :class="isLikeClass" @click="toggleLike">
+				좋아요
+			</a>
 		</div>
 	</div>
 </template>
 
 <script>
-import { ref, useCssModule } from 'vue';
+import { computed } from '@vue/reactivity';
 
-console.log('AppCard module');
 export default {
-	setup() {
-		// const style = useCssModule();
-		// console.log('style: ', style);
-		console.log('AppCard setup()');
-
-		const color = ref('red');
-		// color.value = 'yellow';
-		return { color };
+	props: {
+		type: {
+			type: String,
+			default: 'news',
+			validator: value => {
+				return ['news', 'notice'].includes(value);
+			},
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		contents: {
+			type: String,
+			required: true,
+		},
+		isLike: {
+			type: Boolean,
+			default: false,
+		},
+		obj: {
+			type: Object,
+			default: () => ({}),
+		},
+	},
+	setup(props, { emit }) {
+		console.log('props.title: ', props.title);
+		const isLikeClass = computed(() =>
+			props.isLike ? 'btn-danger' : 'btn-outline-danger',
+		);
+		const typeName = computed(() =>
+			props.type === 'news' ? '뉴스' : '공지사항',
+		);
+		const toggleLike = () => {
+			// props.isLike = !props.isLike;
+			// props.obj.title = '김길동';
+			// emit('changeTitle')
+			emit('toggleLike', props.isLike);
+		};
+		return { isLikeClass, typeName, toggleLike };
 	},
 };
 </script>
 
-<style>
-.red {
-	color: v-bind(color) !important;
-}
-</style>
-<!-- <style module="classes">
-.red {
-	color: red !important;
-}
-</style> -->
-<!-- <style scoped>
-.red {
-	color: red !important;
-}
-</style> -->
+<style></style>
--- src/components/TheView.vue
+++ src/components/TheView.vue
@@ -2,23 +2,15 @@
 	<main>
 		<div class="container py-4">
 			<div class="row g-3">
-				<div class="col col-4">
-					<AppCard></AppCard>
-				</div>
-				<div class="col col-4">
-					<AppCard></AppCard>
-				</div>
-				<div class="col col-4">
-					<AppCard></AppCard>
-				</div>
-				<div class="col col-4">
-					<AppCard></AppCard>
-				</div>
-				<div class="col col-4">
-					<AppCard></AppCard>
-				</div>
-				<div class="col col-4">
-					<AppCard></AppCard>
+				<div v-for="post in posts" :key="post.id" class="col col-4">
+					<AppCard
+						:title="post.title"
+						:contents="post.contents"
+						:type="post.type"
+						:is-like="post.isLike"
+						@toggle-like="value => (post.isLike = !value)"
+						:obj="obj"
+					></AppCard>
 				</div>
 			</div>
 		</div>
@@ -27,12 +19,36 @@
 
 <script>
 import AppCard from '@/components/AppCard.vue';
+import { reactive } from 'vue';
 export default {
 	components: {
 		AppCard,
 	},
 	setup() {
-		return {};
+		const obj = reactive({
+			title: '제목2',
+			contents: '내용2',
+		});
+		const posts = reactive([
+			{ id: 1, title: '제목1', contents: '내용1', isLike: true, type: 'news' },
+			{ id: 2, title: '제목2', contents: '내용2', isLike: true, type: 'news' },
+			{ id: 3, title: '제목3', contents: '내용3', isLike: true, type: 'news' },
+			{
+				id: 4,
+				title: '제목4',
+				contents: '내용4',
+				isLike: false,
+				type: 'notice',
+			},
+			{
+				id: 5,
+				title: '제목5',
+				contents: '내용5',
+				isLike: false,
+				type: 'notice',
+			},
+		]);
+		return { obj, posts };
 	},
 };
 </script>
