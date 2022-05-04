310861c94302116f092ada79ea36b3d83a9a8242
gymcoding <bruce.lean17@gmail.com>
2022-05-04 16:24:45 +0900

    22_slots_2

---
+++ src/components/FancyButton.vue
@@ -0,0 +1,25 @@
+<template>
+	<button class="fancy-btn">
+		<slot><span>!!</span>Default Click!!</slot>
+	</button>
+</template>
+
+<script>
+export default {
+	setup() {
+		return {};
+	},
+};
+</script>
+
+<style scoped>
+.fancy-btn {
+	color: #fff;
+	background: linear-gradient(315deg, #42d392 25%, #647eff);
+	border: none;
+	padding: 5px 12px;
+	margin: 5px;
+	border-radius: 8px;
+	cursor: pointer;
+}
+</style>
