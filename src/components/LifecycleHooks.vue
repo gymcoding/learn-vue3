79f3408e6faaa25240c3f5f710756d879d46da6a
gymcoding <bruce.lean17@gmail.com>
2022-05-06 17:46:00 +0900

    24_lifecycle_hooks_1

---
+++ src/components/LifecycleHooks.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="container py-4"></div>
+</template>
+
+<script>
+export default {
+	setup() {
+		console.log('setup');
+		return {};
+	},
+	// data: () => ({
+	// 	dataMessage: 'data message',
+	// }),
+	// beforeCreate() {
+	// 	console.log('beforeCreate', this.dataMessage);
+	// },
+	// created() {
+	// 	console.log('created', this.dataMessage);
+	// 	console.log(this);
+	// },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/TheView.vue
+++ src/components/TheView.vue
@@ -1,15 +1,13 @@
 <template>
 	<main>
-		<ProvideInject></ProvideInject>
+		<LifecycleHooks></LifecycleHooks>
 	</main>
 </template>
 
 <script>
-import ProvideInject from './ProvideInject.vue';
+import LifecycleHooks from '@/components/LifecycleHooks.vue';
 export default {
-	components: {
-		ProvideInject,
-	},
+	components: { LifecycleHooks },
 	setup() {
 		return {};
 	},
