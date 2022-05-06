d4d9885e7c3c0ba3dc36a9df6923f81a3bcea804
gymcoding <bruce.lean17@gmail.com>
2022-05-06 18:06:41 +0900

    24_lifecycle_hooks_2

---
+++ src/components/LifecycleChild.vue
@@ -0,0 +1,22 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { onBeforeMount, onMounted } from 'vue';
+
+export default {
+	setup() {
+		console.log('[Child] setup');
+		onBeforeMount(() => {
+			console.log('[Child] onBeforeMount');
+		});
+		onMounted(() => {
+			console.log('[Child] onMounted');
+		});
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/LifecycleHooks.vue
+++ src/components/LifecycleHooks.vue
@@ -1,12 +1,27 @@
 <template>
-	<div class="container py-4"></div>
+	<div class="container py-4">
+		<input ref="inputRef" type="text" value="hello world!" />
+		<LifecycleChild></LifecycleChild>
+	</div>
 </template>
 
 <script>
+import { onBeforeMount, onMounted, ref } from 'vue';
+import LifecycleChild from './LifecycleChild.vue';
 export default {
+	components: {
+		LifecycleChild,
+	},
 	setup() {
 		console.log('setup');
-		return {};
+		const inputRef = ref(null);
+		onBeforeMount(() => {
+			console.log('onBeforeMount', inputRef.value);
+		});
+		onMounted(() => {
+			console.log('onMounted', inputRef.value.value);
+		});
+		return { inputRef };
 	},
 	// data: () => ({
 	// 	dataMessage: 'data message',
