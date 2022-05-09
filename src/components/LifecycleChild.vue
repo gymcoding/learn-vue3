e3e44043942f44b8216bdfda232e01805f6cbdcd
gymcoding <bruce.lean17@gmail.com>
2022-05-09 09:02:50 +0900

    24_lifecycle_hooks_4

--- src/components/LifecycleChild.vue
+++ src/components/LifecycleChild.vue
@@ -1,18 +1,29 @@
 <template>
-	<div></div>
+	<div class="card">
+		<div class="card-body">Lifecycle Child</div>
+		<input id="input" type="text" />
+	</div>
 </template>
 
 <script>
-import { onBeforeMount, onMounted } from 'vue';
+import { onBeforeMount, onBeforeUnmount, onMounted, onUnmounted } from 'vue';
 
 export default {
 	setup() {
-		console.log('[Child] setup');
+		// console.log('[Child] setup');
 		onBeforeMount(() => {
 			console.log('[Child] onBeforeMount');
 		});
 		onMounted(() => {
 			console.log('[Child] onMounted');
+		});
+		onBeforeUnmount(() => {
+			console.log('[Child] onBeforeUnmount');
+			console.log(document.querySelector('#input'));
+		});
+		onUnmounted(() => {
+			console.log('[Child] onUnmounted');
+			console.log(document.querySelector('#input'));
 		});
 		return {};
 	},
--- src/components/LifecycleHooks.vue
+++ src/components/LifecycleHooks.vue
@@ -1,22 +1,25 @@
 <template>
 	<div class="container py-4">
 		<input ref="inputRef" type="text" value="hello world!" />
-		<!-- <LifecycleChild></LifecycleChild> -->
+		<hr />
+		<button @click="visible = !visible">Toggle Child</button>
+		<LifecycleChild v-if="visible"></LifecycleChild>
 		<p id="message">{{ message }}</p>
 	</div>
 </template>
 
 <script>
 import { onBeforeMount, onBeforeUpdate, onMounted, onUpdated, ref } from 'vue';
-// import LifecycleChild from './LifecycleChild.vue';
+import LifecycleChild from './LifecycleChild.vue';
 export default {
 	components: {
-		// LifecycleChild,
+		LifecycleChild,
 	},
 	setup() {
 		// console.log('setup');
 		const inputRef = ref(null);
 		const message = ref('');
+		const visible = ref(false);
 		// onBeforeMount(() => {
 		// 	console.log('onBeforeMount', inputRef.value);
 		// });
@@ -24,21 +27,21 @@ export default {
 		// 	console.log('onMounted', inputRef.value.value);
 		// });
 
-		onBeforeUpdate(() => {
-			console.log('onBeforeUpdate', message.value);
-			console.log(
-				'DOM Content: ',
-				document.querySelector('#message').textContent,
-			);
-		});
-		onUpdated(() => {
-			console.log('onUpdated', message.value);
-			console.log(
-				'DOM Content: ',
-				document.querySelector('#message').textContent,
-			);
-		});
-		return { inputRef, message };
+		// onBeforeUpdate(() => {
+		// 	console.log('onBeforeUpdate', message.value);
+		// 	console.log(
+		// 		'DOM Content: ',
+		// 		document.querySelector('#message').textContent,
+		// 	);
+		// });
+		// onUpdated(() => {
+		// 	console.log('onUpdated', message.value);
+		// 	console.log(
+		// 		'DOM Content: ',
+		// 		document.querySelector('#message').textContent,
+		// 	);
+		// });
+		return { inputRef, message, visible };
 	},
 	// data: () => ({
 	// 	dataMessage: 'data message',
