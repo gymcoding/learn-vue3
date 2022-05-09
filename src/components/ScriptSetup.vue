00ef0486f4b2ac437dd4f9487c4ba6447a8221b1
gymcoding <bruce.lean17@gmail.com>
2022-05-09 14:26:34 +0900

    26_script_setup_2

--- src/components/ScriptSetup.vue
+++ src/components/ScriptSetup.vue
@@ -11,18 +11,26 @@
 			contents="내용"
 			:is-like="true"
 		></PostItem>
+		<hr />
+		<TemplateRefsChild ref="child"></TemplateRefsChild>
+		<template v-if="child">{{ child.message }}</template>
 	</div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 import PostItem from '@/components/setup/PostItem.vue';
+import TemplateRefsChild from './setup/TemplateRefsChild.vue';
 
 const msg = 'Hello World!';
 const message = ref('');
 const sayHello = () => {
 	alert('Hello World!');
 };
+const child = ref(null);
+defineExpose({
+	msg,
+});
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/components/setup/TemplateRefsChild.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		{{ $parent }}
+		<ul>
+			<li v-for="fruit in $parent.fruits" :key="fruit">{{ fruit }}</li>
+		</ul>
+	</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const message = ref('Hello ~!');
+const sayHello = () => {
+	alert(message.value);
+};
+defineExpose({
+	message,
+	sayHello,
+});
+</script>
+
+<style lang="scss" scoped></style>
