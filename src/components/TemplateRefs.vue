677bc461da86a0e5b8c9244cea701ca0cc5b7c40
gymcoding <bruce.lean17@gmail.com>
2022-05-09 09:56:42 +0900

    25_template_refs_2

--- src/components/TemplateRefs.vue
+++ src/components/TemplateRefs.vue
@@ -1,11 +1,24 @@
 <template>
-	<div class="container py-4"></div>
+	<div class="container py-4">
+		<input ref="input" type="text" />
+		<p>{{ input }}</p>
+		<p v-if="input">
+			{{ input.value }}, {{ $refs.input.value }}, {{ $refs.input === input }}
+		</p>
+	</div>
 </template>
 
 <script>
+import { onMounted, ref } from 'vue';
 export default {
 	setup() {
-		return {};
+		const input = ref(null);
+		console.log('setup: ', input.value);
+		onMounted(() => {
+			input.value.value = 'Hello World!';
+			console.log('onMounted: ', input.value);
+		});
+		return { input };
 	},
 };
 </script>
